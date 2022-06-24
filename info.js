aea943f9389a43ec37ea5259b6b4195f9953e1b4
Ohwoevwo Meshack <ohwoevwomeshack@gmail.com>
2022-06-24 06:52:46 +0100

    first commit

---
+++ info.js
@@ -0,0 +1,11 @@
+function info() {
+    var name = "Ohwoevwo Meshack";
+    var height = "5.3 feet";
+    var country = "Nigeria";
+
+    document.write("Name: ", name + "<br/>");
+    document.write("Height: ", height + "<br/>");
+    document.write("Country: ", country);
+}
+
+info();
